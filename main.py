72773c31f3b989527f8db84ccec5fef54affa071
“Keren <kerencouton@gmail.com>
2023-12-29 19:52:08 +0100

    Test files

---
+++ main.py
@@ -0,0 +1,63 @@
+import json
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+db = client['TestScraping']
+
+Joueurs=db["Joueurs"]
+Champions=db["Champions"]
+Ranked=db["Ranked"]
+MostChampPlayed=db['MostChampPlayed']
+
+with open('./Test/output.json', 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+    #print(data.keys())
+
+info_joueur=data.get("props", {}).get("pageProps", {}).get("data",{})
+
+rank = info_joueur["ladder_rank"].get("rank")
+total = info_joueur["ladder_rank"].get("total")
+
+if rank is not None and total is not None:
+    pourcentage_rank_total = (rank / total) * 100
+else: pourcentage_rank_total=None
+
+# Nettoyage des Données
+
+data_joueur = {
+    "summoner_id": info_joueur.get("summoner_id"),
+    "game_name": info_joueur.get("game_name"),
+    "tagline": info_joueur.get("tagline"),
+    "level": info_joueur.get("level"),
+    "ladder_rank": pourcentage_rank_total,
+    "profile_image_url": info_joueur.get("profile_image_url")
+}
+
+# TESTS :
+
+print(info_joueur.get("game_name"),info_joueur.get("tagline"),info_joueur.get("level"))
+#profile_image_url
+#level
+#updated_at # JOUEURS
+#print(info_joueur.get("summoner_id"))
+
+lp=info_joueur.get("lp_histories",[])
+#print(lp[0])
+#print(lp) # LP HISTORY
+
+lp_stats=info_joueur.get("league_stats",[])
+print(lp_stats[0].get("queue_info")) # SOLORANKED nom
+#print(lp_stats)
+
+champ_played_most=info_joueur.get("most_champions",{})
+print(champ_played_most.keys())
+print(champ_played_most.get("season_id"))
+#print(champ_played_most.get("champion_stats")[0]) # les champions les plus joués par le joueur
+print(info_joueur.get("ladder_rank")) #JOUEURS
+
+#print(info_joueur.get("champions")[0]) # LISTE DES CHAMPIONS
+
+#games=data.get("props", {}).get("pageProps", {}).get("games",{}).get("data",[])
+#print(games[0].get("participants",[])[1])
+
+
